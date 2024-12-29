5beebdb093527294cdba39f4806d3571a76a5e5a
Freddie Mac - KD5FMU <fred.mcguire.jr@gmail.com>
2024-12-29 09:48:36 -0600

    Update shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Filename: /etc/asterisk/local/reboot_server.sh
-# Purpose: Reboots the server when triggered by a DTMF command.
-# Author: Freddie Mac - KD5FMU / Ham Radio Crusader
+# Filename: /etc/asterisk/local/shutdown_server.sh
+# Purpose: Shuts down the server when triggered by a DTMF command.
+# Author: Ham Radio Crusader
 
-# Log the reboot action for debugging purposes
-echo "$(date): Reboot command received via DTMF." >> /var/log/asterisk/reboot_command.log
+# Log the shutdown action for debugging purposes
+echo "$(date): Shutdown command received via DTMF." >> /var/log/allstar/shutdown_command.log
 
-# Reboot the system
-/sbin/reboot
+# Shutdown the system
+/sbin/shutdown now
